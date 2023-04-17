a4e6cb683529e8b1217f6c3afdb0e5ca1789d536
Pedro Franch <pedrofe.av@gmail.com>
2023-04-17 10:01:27 +0200

    menu de la aplicacion

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -11,46 +11,76 @@ public class Calculadora {
 		int num1;
 		int num2;
 		int resul;
+		int respuesta=0;		
 		
-		//Implementacion suma
+		String aux;		
+	
 		
-		String aux;
-		
-		System.out.println("Indique el primer número");
-		aux=sc.nextLine();
-		num1=Integer.parseInt(aux);
-		
-		System.out.println("Indique el segundo número");
-		aux=sc.nextLine();
-		num2=Integer.parseInt(aux);
-		
-		resul = num1+num2;
-		
-		System.out.println("La suma es " +resul);
-		
-		//Implementacion resta
-		
-		resul = num1-num2;
-		
-		System.out.println("La resta es " +resul);
-		
-		//Implementacion multiplicación 
-		
-		resul = num1*num2;
-		
-		System.out.println("La multiplicación es " +resul);
-		
-		//Implementacion division 
-		
-		resul = num1/num2;
-		
-		System.out.println("La división es " +resul);
-		
-		//Implementacion resto 
-		
-		resul = num1%num2;
-		
-		System.out.println("El resto es de la divisón es " +resul);
+		do {
+			System.out.println("-1 sumar");
+			System.out.println("-2 restar");
+			System.out.println("-3 multiplicar");
+			System.out.println("-4 dividir");
+			System.out.println("-n salir");
+			aux = sc.nextLine();
+			respuesta = Integer.parseInt(aux);
+			switch(respuesta) {
+				case 1:
+					System.out.println("Indique el primer número");
+					aux=sc.nextLine();
+					num1=Integer.parseInt(aux);
+					
+					System.out.println("Indique el segundo número");
+					aux=sc.nextLine();
+					num2=Integer.parseInt(aux);
+					
+					resul = num1+num2;
+					System.out.println("La suma es " +resul);
+					break;
+				case 2:
+					System.out.println("Indique el primer número");
+					aux=sc.nextLine();
+					num1=Integer.parseInt(aux);
+					
+					System.out.println("Indique el segundo número");
+					aux=sc.nextLine();
+					num2=Integer.parseInt(aux);
+					
+					resul = num1-num2;
+					System.out.println("La resta es " +resul);
+					break;
+				case 3:
+					System.out.println("Indique el primer número");
+					aux=sc.nextLine();
+					num1=Integer.parseInt(aux);
+					
+					System.out.println("Indique el segundo número");
+					aux=sc.nextLine();
+					num2=Integer.parseInt(aux);
+					
+					resul = num1*num2;
+					
+					System.out.println("La multiplicación es " +resul);
+					
+					break;
+				case 4:
+					System.out.println("Indique el primer número");
+					aux=sc.nextLine();
+					num1=Integer.parseInt(aux);
+					
+					System.out.println("Indique el segundo número");
+					aux=sc.nextLine();
+					num2=Integer.parseInt(aux);
+					
+					resul = num1/num2;
+					
+					System.out.println("La división es " +resul);
+					resul = num1%num2;
+					
+					System.out.println("El resto es de la divisón es " +resul);
+					break;
+			}
+		}while(respuesta!=0);
 	}
 
 }
