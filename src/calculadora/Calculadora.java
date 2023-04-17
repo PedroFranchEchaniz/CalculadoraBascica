e6423fc8130e28bb1eda851c2d9ebb637a65f6c3
Pedro Franch <pedrofe.av@gmail.com>
2023-04-17 09:21:49 +0200

    scanner y variables

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,10 +1,17 @@
 package calculadora;
 
+import java.util.Scanner;
+
 public class Calculadora {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner (System.in);
+		
+		int num1;
+		int num2;
+		
+		
 	}
 
 }
